2361b2a966c09eafda21591b541749a214a00087
shearpaladin <sandy6@ualberta.ca>
2021-10-13 19:06:23 -0600

    added django_on_heroku to setttings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_on_heroku  # top of the file
+
+# ...
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_on_heroku.settings(locals())  # bottom of the file
